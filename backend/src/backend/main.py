5c733b855a46be227a89afb32de9349e1d1cb419
Nuno Silva <nmsilva164@gmail.com>
2025-06-03 21:57:15 +0100

    Data Models

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional
+from datetime import date
 
 app = FastAPI()
+
+# Data models
+class Subscription(BaseModel):
+    service_name: str
+    monthly_price: float
+    category: str
+    starting_date: date
+
+class User(BaseModel):
+    email: EmailStr
+    name: str
+    subscriptions: Optional[List[Subscription]] = []
 
 # Montar a pasta de ficheiros estáticos (por exemplo, a pasta 'frontend')
 app.mount("/static", StaticFiles(directory="/workspaces/SubHub/frontend"), name="static")
@@ -11,3 +26,4 @@ app.mount("/static", StaticFiles(directory="/workspaces/SubHub/frontend"), name=
 @app.get("/")
 def read_index():
     return FileResponse("/workspaces/SubHub/frontend/index.html")
+
